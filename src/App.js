00ab2cf4a24eac71d51bb5542086ae2ae2f833b6
chastity duhaney <chastity_duhaney82@gmail.com>
2019-04-26 11:14:39 -0400

    created stop watch start and stop functionality

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <h1>Stopwatch</h1>
+        <Stopwatch />
+      </div>
+    );
+  }
+
 }
+
+class Stopwatch extends Component {
+  state = {
+    status: false,
+    runningTime: 0
+  };
+  handleClick = () => {
+    this.setState({ runningTime: 5, running: true });
+  };
+  handleReset = () => {
+    this.setState({ runningTime: 0, running: false });
+  };
+  render() {
+    const { status, runningTime } = this.state;
+    return (
+      <div>
+        <p>{runningTime}ms</p>
+        <button onClick={this.handleClick}>{status ? 'Stop' : 'Start'}</button>
+        <button onClick={this.handleReset}>Reset</button>
+      </div>
+    );
+  }
+}
+
+
+
 
 export default App;
